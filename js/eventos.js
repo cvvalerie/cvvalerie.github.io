41b0f91b82a61b4216180ea52c58e2f6ad188c81
cvvalerie <127355415+cvvalerie@users.noreply.github.com>
2023-03-08 22:00:50 -0300

    initial commit

---
+++ js/eventos.js
@@ -0,0 +1,29 @@
+
+
+
+let svgElement = document.getElementsByClassName('nav-bg')
+
+function lFormacion() {
+ 
+    alert('FORMACION ACADEMICA. Presione <escape> para continuar. Click sobre el icono',);
+}
+
+function lProfesional() {
+ 
+    alert('FORMACION PROFESIONA. Presione <escape> para continuar. Click sobre el icono',);
+}
+
+function lVoluntariado() {
+ 
+    alert('VOLUNTARIADOS. Presione <escape> para continuar. Click sobre el icono',);
+}
+
+function lIdioma() {
+
+   alert('IDIOMAS. Presione <escape> para continuar. Click sobre el icono',);
+}
+
+function lContacto() {
+
+    alert('MI CONTACTO. Presione <escape> para continuar. Click sobre el icono',);
+ }
